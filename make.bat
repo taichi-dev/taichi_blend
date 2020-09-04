bf36950c2c02f0bbe62086c423d0108e856c74ce
彭于斌 <17721388340@163.com>
2020-09-04 14:29:38 +0800

    serve win too

---
+++ make.bat
@@ -0,0 +1,9 @@
+del /f /s dist\*.whl
+python setup.py bdist_wheel
+mkdir build\Taichi-Blend
+python -m pip install dist\taichi_blend-0.0.1-py3-none-any.whl -t build\Taichi-Blend
+copy bundle.py build\Taichi-Blend\__init__.py
+del /f /s build\Taichi-Blend.zip
+cd build
+zip -r Taichi-Blend.zip Taichi-Blend
+cd ..
